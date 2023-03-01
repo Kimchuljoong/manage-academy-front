1130671b015ce0bf4f49e72d200b683ac72c44d8
kimchj <wnmh123@gmail.com>
2023-03-01 16:37:57 +0900

    ClassDetailPage add element, add and remove function for curriculum

--- src/pages/ClassDetailPage.js
+++ src/pages/ClassDetailPage.js
@@ -3,7 +3,7 @@
  * created at 2023/02/27
  */
 
-import React from "react";
+import React, { useState } from "react";
 
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
@@ -12,9 +12,23 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 const ClassDetailPage = () => {
+
+    const [teachers, setTeachers] = useState(['a', 'b', 'c'])
+    const [list, setList] = useState([])
+
+    const add = () => {
+        setList([...list, ''])
+    }
+
+    const remove = (targetId) => {
+        setList(list.filter((e, idx) => {
+            return idx !== targetId
+        }))
+    }
+
     return (
         <div>
-            <div className="d-flex justify-content-center">
+            <div className="d-flex justify-content-center mb-3">
                 <h2>수업 등록/수정</h2>
             </div>
             <Form className="">
@@ -25,21 +39,66 @@ const ClassDetailPage = () => {
 
                 <Form.Group className="mb-3" controlId="teacher">
                     <Form.Label>Teacher</Form.Label>
-                    <Form.Control type="text" placeholder="Enter Teacher Name" />
-                </Form.Group>
-
-                <Form.Group className="mb3" controlId="occupancy">
-                    <Form.Label>Occupancy</Form.Label>
-                    <Form.Select size="lg">
-                        <option>0</option>
-                        <option>1</option>
-                        <option>2</option>
+                    <Form.Select size="md">
+                        {teachers.map((teacher, idx) => {
+                            return ( <option value={idx}>{teacher}</option> )
+                        })}
                     </Form.Select>
                 </Form.Group>
 
-                <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                    <Form.Check type="checkbox" label="Check me out" />
+                <Form.Group className="mb-3" controlId="occupancy">
+                    <Form.Label>Occupancy</Form.Label>
+                    <Form.Select size="md">
+                        {[...Array(parseInt(25))].map((n, idx) => {
+                            return ( <option value={idx}>{idx}</option> )
+                        })}
+                    </Form.Select>
                 </Form.Group>
+                
+                <Form.Group className="mb-3" controlId="description">
+                    <Form.Label>Description</Form.Label>
+                    <Form.Control as="textarea" rows={5} />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="curriculum">
+                    <div className="d-flex justify-content-between mb-1">
+                        <Form.Label>Curriculum</Form.Label>
+                        <Button onClick={() => add()}>Add</Button>
+                    </div>
+                    <div className="border rounded" placeholder="">
+                    {
+                        list.map((e, idx) => {
+                            return (
+                                <div className="d-flex">
+                                    <div className="align-middle text-center"> {idx+1}</div>
+                                    <Form.Control
+                                        className="border rounded-0" 
+                                        type="text" 
+                                        placeholder="step" 
+                                        value={e} 
+                                        onChange={
+                                            (e) => {
+                                                list[idx] = e.target.value
+                                                setList([...list])
+                                            }
+                                        }/>
+                                        <Button
+                                            className="btn btn-danger"
+                                            onClick={
+                                                () => remove(idx)
+                                            }>
+                                            remove
+                                        </Button>
+                                </div>)
+                        })
+                    }                        
+                    </div>
+                </Form.Group>
+
+                {/* <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                    <Form.Check type="checkbox" label="Check me out" />
+                </Form.Group> */}
+                
                 <Button variant="primary" type="submit">
                     Submit
                 </Button>
